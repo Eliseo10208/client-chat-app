35ce125e5401cef8908df1a5fec5fe996dbd667f
Eliseo10208 <113640708+Eliseo10208@users.noreply.github.com>
2024-01-21 20:28:58 -0600

    primer commit del cliente

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import {io} from 'socket.io-client'
+import { useState,useEffect } from 'react';
+import Styles from '../src/App.css';
+const socket = io('http://localhost:3000');
 function App() {
+
+  const [isConnected, setIsConnected] = useState();
+  const [nuevoMensaje,setNuevoMensaje] = useState('');
+  const [mensajes, setMensajes] = useState([]);
+
+  useEffect(()=>{
+      socket.on('connect',() => setIsConnected(true));
+      socket.on('chat_message',(data)=>{
+        setMensajes(mensajes => [...mensajes ,data])
+      })
+      return () =>{
+        socket.off('connect');
+        socket.off('chat_message');
+      }
+  },[])
+
+  const enviarMensaje = () =>{
+      socket.emit('chat_message',{
+        usuario: socket.id,
+        mensaje: nuevoMensaje
+      });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className='Main-Container'>
+    <div className='Container' style={{display:'flex',flexDirection:'column'}}>
+      <h2>{isConnected ? 'Conectado': 'Desconectado'}</h2>
+      {mensajes.map(mensaje =>(
+        <label key={mensaje.usuario} className='Usuario'>{mensaje.usuario}: {mensaje.mensaje}</label>
+      ))
+      }
+      <input type='text'
+        onChange={e => setNuevoMensaje(e.target.value)}
+      />
+      <button onClick={enviarMensaje}>Enviar</button>
     </div>
-  );
+      </section>
+  )
 }
 
-export default App;
+export default App
